38cb4bae2daefe6972535b69a4c0cce2d3427a53
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-28 18:58:06 +0330

    fix js

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const frmInt = document.querySelector('#frmInt');
-const city = document.querySelector('#city');
 const desc = document.querySelector('#desc');
 const temp = document.querySelector('#temp');
 const wind = document.querySelector('#wind');
-const cityIat = document.querySelector('#citylat');
-const cityIon = document.querySelector('#citylon');
-const search = document.querySelector('#search')
-const citys=document.querySelector('.city')
+const search = document.querySelector('#search');
+const citys = document.querySelector('.citys');
+const city=document.querySelector('#city')
 const keys = '93e8b2df642d3526ce153214fc999994'
 
 async function getweather() {
-    const reslwc = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citys.value}&appid=${keys}`)).json();
-    const {main:{temp},name,weather:[{description}],wind:{speed}}=reslwc
-    setci(temp, name, description, speed)
+    const resultApi = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citys.value}&appid=${keys}`)).json();
+    const {main:{temp},name,weather:[{description}],wind:{speed}}=resultApi
+     setWeather(temp, name, description, speed)
 }
 
-function setci(data, data1, data2, data3) {
-    city.innerHTML = `city ${data1}`;
+function setWeather(data, data1, data2, data3) {
+    city.innerHTML = `City -  ${data1}`;
     desc.innerHTML = ` ${data2}`
-    temp.innerHTML = `temp   ${ (data -273).toFixed(2)}`;
-    wind.innerHTML = `wind ${data3}`
+    temp.innerHTML = `Temp   ${ (data -273).toFixed(2)}`;
+    wind.innerHTML = `Wind ${data3}`
     citys.value = ' ';
 }
 
