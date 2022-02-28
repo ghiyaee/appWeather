53d50b0184685dd0df05ebe51c0fb3ee3d8e8d53
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-28 18:43:10 +0330

    change api

--- app.js
+++ app.js
@@ -6,29 +6,24 @@ const wind = document.querySelector('#wind');
 const cityIat = document.querySelector('#citylat');
 const cityIon = document.querySelector('#citylon');
 const search = document.querySelector('#search')
-
+const citys=document.querySelector('.city')
 const keys = '93e8b2df642d3526ce153214fc999994'
 
 async function getweather() {
-    const reslw =await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityIat.value}&lon=${cityIon.value}&appid=${keys}`)).json();
-      console.log(reslw);
-       
-    const { main: { temp }, name,weather:[{description}],wind:{speed}} = reslw
-    
-   setin(temp, name, description, speed)
+    const reslwc = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citys.value}&appid=${keys}`)).json();
+    const {main:{temp},name,weather:[{description}],wind:{speed}}=reslwc
+    setci(temp, name, description, speed)
 }
 
-function setin(data,data1,data2,data3) {
+function setci(data, data1, data2, data3) {
     city.innerHTML = `city ${data1}`;
     desc.innerHTML = ` ${data2}`
-    temp.innerHTML =  `temp   ${ (data -273).toFixed(2)}`;
-    wind.innerHTML =  `wind ${data3}`
+    temp.innerHTML = `temp   ${ (data -273).toFixed(2)}`;
+    wind.innerHTML = `wind ${data3}`
+    citys.value = ' ';
 }
 
 search.addEventListener('click', (e) => {
-  
-    getweather()
-     temp.innerHTML=temp
-
-    e.preventDefault()
+    getweather();
+    e.preventDefault();
 })
