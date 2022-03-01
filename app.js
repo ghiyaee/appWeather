42b6f19f50cce3d72c317c741b682ee9c1bbc9c9
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-03-01 12:04:51 +0330

    add transform

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const search = document.querySelector('#search');
 const citys = document.querySelector('.citys');
 const city = document.querySelector('#city');
 const keys = '93e8b2df642d3526ce153214fc999994';
+const h2 = document.querySelector('h2');
+const h3 = document.querySelector('h3');
+const h4 = document.querySelector('h4');
+const h5 = document.querySelector('h5');
+
 const cityArray=[]
+load();
 
 async function getweather() {
     const resultApi = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citys.value}&appid=${keys}`)).json();
@@ -20,13 +26,17 @@ function setWeather(data, data1, data2, data3) {
     temp.innerHTML = `Temp   ${ (data2 -273).toFixed(2)}`;
     wind.innerHTML = `Wind ${data3}`;
     citys.value = ' ';
+    h2.style.transform = `translateX(0)`;
+    h3.style.transform = `translateX(0)`;
+    h4.style.transform = `translateX(0)`;
+    h5.style.transform = `translateX(0)`;
+    
 }
 
 function load() {
     citys.value=JSON.parse(localStorage.getItem('city'))
      getweather()
 }
-load();
 search.addEventListener('click', (e) => {
     getweather();
     if (cityArray == '') {
